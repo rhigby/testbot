167db122361168b5120fb9785579a5301414f3b1
rhigby <ralphhigby@gmail.com>
2025-07-27 21:42:37 -0400

    Update themeLoader.js

--- themeLoader.js
+++ themeLoader.js
@@ -1,1 +1,25 @@
+const fs = require("fs");
+const path = require("path");
+
+const themeCache = {};
+
+function loadTheme(themeName) {
+  if (!themeCache[themeName]) {
+    const filePath = path.join(__dirname, "themes", `${themeName}.json`);
+    const raw = fs.readFileSync(filePath, "utf-8");
+    themeCache[themeName] = JSON.parse(raw);
+  }
+  return themeCache[themeName];
+}
+
+function getWordForLetter(letter, index, themeName = "general") {
+  const theme = loadTheme(themeName);
+  const list = index % 2 === 0 ? theme.adjectives : theme.nouns;
+  const options = list.filter(word => word[0].toUpperCase() === letter.toUpperCase());
+  return options.length > 0
+    ? options[Math.floor(Math.random() * options.length)]
+    : letter;
+}
+
+module.exports = { getWordForLetter };
 
